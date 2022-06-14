1ead4ec57d1e225a98846ea8518d3b912d1dbfdf
Harishankar Bhat R <rhsb1997@gmail.com>
2022-06-14 22:20:14 +0530

    Added Happy Number solution using Floyd Cycle Finding Algorithm

--- Happy Number/src/Solution.java
+++ Happy Number/src/Solution.java
@@ -26,6 +26,16 @@ public class Solution {
         System.out.println("Is 1 a Happy Number? " + isHappy_solution_2(1));
         System.out.println("Is 63 a Happy Number? " + isHappy_solution_2(63));
         System.out.println("Is 19 a Happy Number? " + isHappy_solution_2(19));
+
+        System.out.println("Solution 3");
+
+        System.out.println("Is 32 a Happy Number? " + isHappy_solution_using_floyd_cycle_finding_algorithm(32));
+        System.out.println("Is 31 a Happy Number? " + isHappy_solution_using_floyd_cycle_finding_algorithm(31));
+        System.out.println("Is 7 a Happy Number? " + isHappy_solution_using_floyd_cycle_finding_algorithm(7));
+        System.out.println("Is 2 a Happy Number? " + isHappy_solution_using_floyd_cycle_finding_algorithm(2));
+        System.out.println("Is 1 a Happy Number? " + isHappy_solution_using_floyd_cycle_finding_algorithm(1));
+        System.out.println("Is 63 a Happy Number? " + isHappy_solution_using_floyd_cycle_finding_algorithm(63));
+        System.out.println("Is 19 a Happy Number? " + isHappy_solution_using_floyd_cycle_finding_algorithm(19));
     }
 
     private static boolean isHappy_solution_1(int number) {
@@ -52,6 +62,18 @@ public class Solution {
         }
 
         return number == 1;
+    }
+
+    private static boolean isHappy_solution_using_floyd_cycle_finding_algorithm(int number) {
+        int slowRunner = number;
+        int fastRunner = getNextNumber(number);
+
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNextNumber(slowRunner);
+            fastRunner = getNextNumber(getNextNumber(fastRunner));
+        }
+
+        return fastRunner == 1;
     }
 
     private static int getNextNumber(int number) {
