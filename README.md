7ad2a49991da6e0c864aeae05bb27ee0370d9fe4
Harishankar Bhat R <rhsb1997@gmail.com>
2022-06-28 21:49:54 +0530

    Recursion

--- README.md
+++ README.md
@@ -69,3 +69,4 @@
 #### 67. [Graph - Adjacency Matrix and Adjacency List](https://github.com/Harishankar-GitHub/Other-Coding-Problems/blob/master/Graph%20-%20Adjacency%20Matrix%20and%20Adjacency%20List/src/Solution.java)
 #### 68. [Happy Number](https://github.com/Harishankar-GitHub/Other-Coding-Problems/blob/master/Happy%20Number/src/Solution.java)
 #### 69. [Beautiful Numbers](https://github.com/Harishankar-GitHub/Other-Coding-Problems/blob/master/Beautiful%20Numbers/src/Solution.java)
+#### 70. [Recursion]()
---
+++ Recursion/src/Recursion.java
@@ -0,0 +1,60 @@
+public class Recursion {
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5: " + factorial(5));
+        System.out.println("2 power 5: " + twoPower(5));
+        System.out.println("Print numbers from 5-1:");
+        printFiveToOne(5);
+        System.out.println("Print numbers from 1-5:");
+        printOneToFive(5);
+    }
+
+    private static int factorial(int n) {
+        // 5! = 5 * 4 * 3 * 2 * 1
+        // 5! = 5 * 4!
+        // 4! = 4 * 3!
+        // 3! = 3 * 2!
+        // 2! = 2 * 1!
+        // 1! = 1 * 0!
+        // 0! = 0;      // Base case
+
+        // Base case
+        if (n == 0) return 1;
+
+        return n * factorial(n-1);
+    }
+
+    private static int twoPower(int n) {
+        // 2^5 = 2 * 2^4 = 2 * 16
+        // 2^4 = 2 * 2^3
+        // 2^3 = 2 * 2^2
+        // 2^2 = 2 * 2^1
+        // 2^1 = 2 * 2^0
+        // 2^0 = 1      // Base case
+
+        // Base case
+        if (n == 0) return 1;
+
+        return 2 * twoPower(n-1);
+    }
+
+    private static void printOneToFive(int n) {
+        // Base case
+        if (n == 0) return;
+
+        // Head Recursion - Recursive call comes before processing.
+        printOneToFive(n-1);
+
+        System.out.println(n);
+    }
+
+    private static void printFiveToOne(int n) {
+        System.out.println(n);
+
+        // Base case
+        if (n == 1) return;
+
+        // Tail Recursion - Recursive call comes after processing.
+        printFiveToOne(n-1);
+    }
+}
